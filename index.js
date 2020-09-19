606e44f4addb65f3944562554fea7d8fac6c9413
Andy Hayes <1979@hey.com>
2020-09-19 16:05:18 -0500

    canvas and index.js added

---
+++ index.js
@@ -0,0 +1,20 @@
+//canvas context 
+const canvas = document.getElementById('can');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth/1.05;
+canvas.height = window.innerHeight/1.5;
+
+ctx.fillRect(100, 100, 20, 20);
+ctx.fillRect(800, 500, 20, 40);
+ctx.fillRect(300, 100, 20, 20);
+ctx.fillRect(1070, 200, 20, 20);
+ctx.fillRect(100, 200, 50, 20);
+ctx.fillRect(380, 10, 20, 20);
+ctx.fillRect(900, 260, 20, 20);
+ctx.fillRect(600, 300, 60, 20);
+ctx.fillRect(1600, 200, 70, 20);
+ctx.fillRect(130, 240, 20, 20);
+ctx.fillRect(680, 130, 20, 20);
+ctx.fillRect(200, 130, 60, 60);
+ctx.fillRect(2300, 530, 90, 90);
