3da858e63203b7e84921845879272bcd49b57101
Andy Hayes <1979@hey.com>
2020-09-19 18:53:26 -0500

    canvas to smiley face

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 const canvas = document.getElementById('can');
 const ctx = canvas.getContext('2d');
 
+
 canvas.width = window.innerWidth/1.05;
 canvas.height = window.innerHeight/1.5;
 
-ctx.fillRect(100, 100, 20, 20);
-ctx.fillRect(800, 500, 20, 40);
-ctx.fillRect(300, 100, 20, 20);
-ctx.fillRect(1070, 200, 20, 20);
-ctx.fillRect(100, 200, 50, 20);
-ctx.fillRect(380, 10, 20, 20);
-ctx.fillRect(900, 260, 20, 20);
-ctx.fillRect(600, 300, 60, 20);
-ctx.fillRect(1600, 200, 70, 20);
-ctx.fillRect(130, 240, 20, 20);
-ctx.fillRect(680, 130, 20, 20);
-ctx.fillRect(200, 130, 60, 60);
-ctx.fillRect(2300, 530, 90, 90);
+const centerY = canvas.height/2;
+const centerX = canvas.width/2;
+
+ctx.beginPath();
+ctx.arc(centerX, centerY, 250, 0, Math.PI *2, true);
+ctx.moveTo(centerX + 175, centerY);
+ctx.arc(centerX, centerY, 175, 0, Math.PI, false);
+ctx.moveTo(centerX - 80, centerY -50);
+ctx.arc(centerX -100, centerY-50, 20, 0, Math.PI * 2);
+ctx.moveTo(centerX + 100, centerY - 50);
+ctx.arc(centerX +80, centerY-50, 20, 0, Math.PI * 2);
+
+ctx.stroke();
